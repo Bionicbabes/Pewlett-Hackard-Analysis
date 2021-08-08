b5b871e4d308811a68935c7076b8c1ac3f165b01
Bionicbabes <85971908+Bionicbabes@users.noreply.github.com>
2021-08-08 16:22:08 -0500

    Add files via upload

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,4 +1,5 @@
 -- Deliverable 1
+-- Retirement_titles table
 SELECT e.emp_no,
 	e.first_name,
 	e.last_name,
@@ -57,3 +58,52 @@ ON e.emp_no = t.emp_no
 WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31') AND de.to_date = '9999-01-01'
 ORDER BY e.birth_date ASC
 ; 
+
+-- Summary 
+-- How many roles will need to be filled as the "silver tsunami" begins to make an impact?
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	t.title,
+	t.from_date,
+	t.to_date,
+	e.birth_date
+INTO retirement_titles_52
+FROM employees as e
+INNER JOIN titles as t 
+ON e.emp_no = t.emp_no
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1952-12-31')
+ORDER BY e.birth_date DESC;
+
+SELECT DISTINCT ON (rts.emp_no) rts.emp_no,
+	rts.first_name,
+	rts.last_name,
+	rts.title
+INTO unique_titles_52
+FROM retirement_titles_52 as rts
+ORDER BY rts.emp_no, rts.from_date DESC;
+
+SELECT COUNT(uts.title), 
+	uts.title
+INTO retiring_titles_52
+FROM unique_titles_52 as uts
+GROUP BY uts.title
+ORDER BY COUNT(uts.title) DESC;
+
+--Are there enough qualified, retirement-ready employees in the departments to mentor the next generation of Pewlett Hackard employees?
+
+SELECT ut.emp_no,
+	ut.first_name,
+	ut.last_name,
+	di.dept_name
+INTO retirement_dept
+FROM unique_titles as ut
+INNER JOIN dept_info as di 
+ON ut.emp_no = di.emp_no;
+
+SELECT COUNT(rd.dept_name), 
+	rd.dept_name
+INTO retiring_dept
+FROM retirement_dept as rd
+GROUP BY rd.dept_name
+ORDER BY COUNT(rd.dept_name) DESC;
